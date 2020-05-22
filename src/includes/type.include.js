7454d93099f110527398f84c0f0a437595d830de
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-05-22 14:05:04 -0400

    📝 adds type checking

--- src/includes/type.include.js
+++ src/includes/type.include.js
@@ -1,3 +1,7 @@
+// @ts-check
+/**
+ * @param {any} obj - an object thats type cannot be normally derived with typeof
+ */
 export default function (obj) {
 	return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
 }
