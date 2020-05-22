b7e7239f653f414fc92d8b580cfec6fe21c067fd
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-05-22 14:05:58 -0400

    📝 adds ts-check - and notes
    💄 adds import of type() for more precise type checking
    - and worker options

--- src/includes/worker.include.js
+++ src/includes/worker.include.js
@@ -1,10 +1,20 @@
+// @ts-check
+import type from './type.include';
 // yyx990803/workerify - https://github.com/yyx990803/workerify/
-export default function (func) {
-	if (typeof func !== 'function') {
+/**
+ * Workerify - when passed an anonymous function, will run said function in its own thread.
+ * Now the function will not impact the performance of the main thread,
+ * allowing it to focus on more important things that it is designed for,
+ * like UI and interactions
+ * @param {Function} func - anonymouns function to stringify into an inline worker
+ * @param {object} options - an object of valid worker options
+ */
+export default function (func, options = {}) {
+	if (type(func) !== 'function') {
 		throw new Error('expects a function to workerify.');
 	}
 	var script = func.toString().match(/^function[^{]*{((.|\n)*)}$/)[1],
 		blob = new Blob([script], { type: 'application/javascript' }),
 		url = window.URL.createObjectURL(blob);
-	return new Worker(url);
+	return new Worker(url, options);
 }
