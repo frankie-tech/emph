56c62d8d74eca975cb6bfeaea68138019a19de52
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-05-27 19:04:10 -0400

    ✏️ Wrote a whole settings plugin
    Adds a proxy for a Map object
    
    Idea is that certain settings are saved to the Map
    object in a key value pair only to be accessed through
    the proxy object. The map is hidden in the prototype
    behind a symbol to make it harder to access
    they are assigned by prefixing the names with an underscore
    
    Next is to add a "guard" to the delete property function, so that
    the settings are protected

--- src/includes/settings.include.js
+++ src/includes/settings.include.js
@@ -1,13 +1,65 @@
-const Settings = (() => {
-	function Context(instance, key, proxyTarget, proxyHandler) {
-		this.target = proxyTarget || {};
-		this.handler = proxyHandler || {};
-		const proxy = new Proxy(this.target, this.handler);
-		instance.settings[key] = proxy;
-		return proxy;
-	}
+import type from './type.include';
+import { err } from './message.include';
 
-	Context.prototype.createHandler = function () {};
+const Settings = (function () {
+	/**
+	 *
+	 * @param {Emph} instance - this would be the Emph or Javascript function/prototype that you are storing settings for
+	 * @param {object} baseObject - default object, either fill with custom settings of your own but will default to empty object
+	 */
+	function Context(instance, baseObject) {
+		const _this = instance;
+		const self = this;
+		self.store = _this.prototype[Symbol('_store')] = new Map();
+
+		const data = function () {
+			return {
+				get: function (obj, key) {
+					/* for now does not support nested objects/arrays
+                    if (['object', 'array'].indexOf(type(obj[key])) > -1) {
+                        return new Proxy(obj[key], data(instance));
+                    }
+                    */
+					if (key.indexOf('_') === 0) return undefined;
+					return obj[key];
+				},
+				set: function (obj, key, value) {
+					if (obj[key] === value) return true;
+					if (key.indexOf('_') === 0) {
+						return self.store.set(key, value);
+					}
+					console.log(key);
+					obj[key] = value;
+					return true;
+				},
+				deleteProperty: function (obj, key) {
+					// looks for a secret key just in case;
+					if (key.indexOf('_') === 0) {
+						self.store.delete(key);
+						return true;
+					}
+					if (key in obj) {
+						delete obj[key];
+						return true;
+					}
+				},
+			};
+		};
+
+		const dataHandler = data(instance);
+		this.settings = _this.prototype[Symbol('_settings')] = new Proxy(
+			baseObject,
+			dataHandler
+		);
+
+		return this.settings;
+	}
+	/*
+	Context.prototype.createTarget = function (target) {
+		if (type(target) !== 'object')
+			throw err('@param:target needs to be of type Object');
+    };
+    */
 
 	return Context;
 })();
