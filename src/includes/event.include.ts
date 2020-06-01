edddb21ddce1d10d6d47a9710ae9ff884fee195e
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-01 18:24:16 -0400

    💄 cleaned out old comments

--- src/includes/event.include.ts
+++ src/includes/event.include.ts
@@ -1,6 +1,3 @@
-// @ts-check
-'use strict';
-
 type eventType = string;
 type eventHandlers = Array<Function>;
 type EmphHandlers = Map<eventType, eventHandlers>;
@@ -30,57 +27,3 @@ export default class EmphEvents {
 		eventHandlersForAny.slice().map(handler => handler(eventType, event))
 	}
 }
-/*
-export default function () {
-	const self = this;
-
-	function init() {
-		self.handlers = self.handlers || new Map();
-
-		/**
-		 *
-		 * @param {string} eventType - custom event name
-		 * @param {Function} eventHandler - callback function to run when event is emitted
-
-		const on = function (eventType, eventHandler) {
-			const eventHandlers = self.handlers.get(eventType);
-			const added = eventHandlers && eventHandlers.push(eventHandler);
-			if (!added) {
-				self.handlers.set(eventType, [eventHandler]);
-			}
-		};
-
-		/**
-		 *
-		 * @param {string} eventType - custom event name
-		 * @param {Function} eventHandler - callback function to run when event is emitted
-
-		const off = function (eventType, eventHandler) {
-			const eventHandlers = self.handlers.get(eventType);
-			if (eventHandlers) {
-				eventHandlers.splice(eventHandlers.indexOf(eventHandler) >>> 0, 1);
-			}
-		};
-
-		/**
-		 *
-		 * @param {string} eventType - event name to emit
-		 * @param {object} event - object to pass to the handler function
-
-		const emit = function (eventType, event) {
-			const eventHandlerOfType = self.handlers.get(eventType) || [];
-			const eventHandlerAny = self.handlers.get('*') || [];
-
-			eventHandlerOfType.slice().map((handler) => handler(event));
-			eventHandlerAny.slice().map((handler) => handler(eventType, event));
-		};
-
-		return {
-			on,
-			off,
-			emit,
-		};
-	}
-	return init;
-}
-*/
