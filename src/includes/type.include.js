ecb5ab01ac08e1421bf36e740bde448bbce1a6da
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-01 17:44:48 -0400

    💄 converts to typescript
    is also extracted to core class, maybe this
    can be deleted entirely?

--- src/includes/type.include.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-/**
- * @param {any} obj - an object thats type cannot be normally derived with typeof
- */
-export default function (obj) {
-	return Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
-}
---
+++ src/includes/type.include.ts
@@ -0,0 +1,4 @@
+// returns the true type of a variable
+export default function (variable: any): string {
+    return Object.prototype.toString.call(variable).slice(8, -1).toLowerCase();
+}
