c88a82953f17fdf4c0de890514074518667b123d
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-01 18:01:20 -0400

    🛠 adds requestIdle to window interface

--- src/type.d.ts
+++ src/type.d.ts
@@ -3,7 +3,18 @@ declare global {
     interface Window {
         _: _;
         emph: _;
+        requestIdleCallback: ((callback: ((deadline: requestIdleCallbackDeadline) => void), opts?: RequestIdleCallbackOptions, ) => RequestIdleCallbackHandle);
+        cancelIdleCallback: ((handle: RequestIdleCallbackHandle) => void);
     }
 }
+
+type RequestIdleCallbackHandle = any;
+type RequestIdleCallbackOptions = {
+    timeout: number;
+};
+type requestIdleCallbackDeadline = {
+    readonly didTimeout: boolean;
+    timeRemaining: (() => number);
+};
 
 export default global;
