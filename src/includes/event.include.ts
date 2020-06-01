71b492b84b213b9b7f9c3673021d6825254a0737
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-01 19:00:33 -0400

    💄 moved dom listener utils to EmphEvents
    Makes sense to compile those two concepts
    together since they are apart of the same
    mental model

--- src/includes/event.include.ts
+++ src/includes/event.include.ts
@@ -3,32 +3,48 @@ type eventHandlers = Array<Function>;
 type EmphEmitters = Map<eventType, eventHandlers>;
 
 export default class EmphEvents {
-	handlers: EmphEmitters;
+    handlers: EmphEmitters;
 
-	constructor() {
-		this.handlers = this.handlers || new Map();
-	}
+    constructor() {
+        this.handlers = this.handlers || new Map();
+    }
 
-	on(eventType: string, eventHandler: Function): void {
-		let currentHandlers = this.handlers.get(eventType);
-		let hasCurrentEvent = currentHandlers.push(eventHandler);
-		if (!hasCurrentEvent) {
-			this.handlers.set(eventType, [eventHandler]);
-		}
-	}
+    listen(eventName: string, eventTarget: Document | Window | HTMLElement, eventListener: EventListener, options: EventListenerOptions): void {
+        if (!eventTarget) {
+            document.addEventListener(eventName, eventListener, options);
+        }
 
-	off(eventType: string, eventHandler: Function): void {
-		let currentHandlers = this.handlers.get(eventType);
-		if (currentHandlers) {
-			currentHandlers.splice(currentHandlers.indexOf(eventHandler) >>> 0, 1);
-		}
-	}
+        eventTarget.addEventListener(eventName, eventListener, options);
+    }
 
-	emit(eventType: string, event: {}): void {
-		let eventHandlersForType = this.handlers.get(eventType) || [];
-		let eventHandlersForAny = this.handlers.get('*') || [];
+    ready(eventListener: EventListener, win: boolean): void {
+        if (!win) {
+            document.addEventListener('DOMContentLoaded', eventListener, { capture: true, once: true });
+            return;
+        }
+        window.addEventListener('load', eventListener, { once: true });
+    }
 
-		eventHandlersForType.slice().map(handler => handler(event));
-		eventHandlersForAny.slice().map(handler => handler(eventType, event))
-	}
+    on(eventType: string, eventHandler: Function): void {
+        let currentHandlers = this.handlers.get(eventType);
+        let hasCurrentEvent = currentHandlers.push(eventHandler);
+        if (!hasCurrentEvent) {
+            this.handlers.set(eventType, [eventHandler]);
+        }
+    }
+
+    off(eventType: string, eventHandler: Function): void {
+        let currentHandlers = this.handlers.get(eventType);
+        if (currentHandlers) {
+            currentHandlers.splice(currentHandlers.indexOf(eventHandler) >>> 0, 1);
+        }
+    }
+
+    emit(eventType: string, event: {}): void {
+        let eventHandlersForType = this.handlers.get(eventType) || [];
+        let eventHandlersForAny = this.handlers.get('*') || [];
+
+        eventHandlersForType.slice().map(handler => handler(event));
+        eventHandlersForAny.slice().map(handler => handler(eventType, event))
+    }
 }
