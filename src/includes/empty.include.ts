624b9b241817ca020d51690637e17239c2dd0a3e
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-01 17:44:09 -0400

    💄 converts empty script to typescript

---
+++ src/includes/empty.include.ts
@@ -0,0 +1,9 @@
+// checks if the variable is equal to a false/empty value
+export default function (variable: any): boolean {
+    if ([undefined, null, false, 0, '', '0'].includes(variable)) return true;
+    if (variable.pop) return variable.length === 0 ? true : false;
+    if (variable instanceof Object)
+        return Object.entries(variable).length === 0 ? true : false;
+
+    return false;
+}
