f1a4b9614704f44682746f0f96ea9372f9410b83
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-01 18:33:23 -0400

    cleaned up a comment

--- src/includes/worker.include.ts
+++ src/includes/worker.include.ts
@@ -5,21 +5,19 @@
  * Now the function will not impact the performance of the main thread,
  * allowing it to focus on more important things that it is designed for,
  * like UI and interactions
- * @param {Function} func - anonymouns function to stringify into an inline worker
- * @param {object} options - an object of valid worker options
  */
 function tostring(func: Function): string {
-	return func.toString().match(/^function[^{]*{((.|\n)*)}$/)[1];
+    return func.toString().match(/^function[^{]*{((.|\n)*)}$/)[1];
 }
 function toblob(str: string): Blob {
-	return new Blob([str], { type: 'application/javascript' });
+    return new Blob([str], { type: 'application/javascript' });
 }
 function tourl(blob: Blob): string {
-	return window.URL.createObjectURL(blob);
+    return window.URL.createObjectURL(blob);
 }
 export default function (func: Function, options: {}): Worker {
-	const str = tostring(func);
-	const blob = toblob(str);
-	const url = tourl(blob)
-	return new Worker(url, options);
+    const str = tostring(func);
+    const blob = toblob(str);
+    const url = tourl(blob)
+    return new Worker(url, options);
 }
