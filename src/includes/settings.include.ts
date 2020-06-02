154f8c064ed98c6ca960c4d70075978dca78ce9f
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-01 21:10:41 -0400

    📝 removed comments

--- src/includes/settings.include.ts
+++ src/includes/settings.include.ts
@@ -1,57 +1,4 @@
-/*
-const Settings = (function () {
-	function Context(instance: any, baseObject: {}) {
-		const _this = instance;
-		const self = this;
-		self.store = _this.prototype[Symbol('_store')] = new Map();
-
-		const data = function (): ProxyHandler<object> {
-			return {
-				get: function (obj: { [key: string]: string | number }, key: string): undefined | string | number {
-					// for now does not support nested objects/arrays
-					// if (['object', 'array'].indexOf(type(obj[key])) > -1) {
-					// return new Proxy(obj[key], data(instance));
-					// }
-
-					if (key.indexOf('_') === 0) return undefined;
-					return obj[key];
-				},
-				set: function (obj: { [key: string]: string | number }, key: string, value: string | number): boolean {
-					if (obj[key] === value) return true;
-					if (key.indexOf('_') === 0) {
-						return self.store.set(key, value);
-					}
-					obj[key] = value;
-					return true;
-				},
-				deleteProperty: function (obj: { [key: string]: string | number }, key: string) {
-					// looks for a secret key just in case;
-					if (key.indexOf('_') === 0) {
-						return self.store.has(key) ? self.store.delete(key) : false;
-					}
-					if (key in obj) {
-						delete obj[key];
-						return true;
-					}
-					return false;
-				},
-			};
-		};
-
-		const dataHandler = data();
-		this.settings = _this.prototype[Symbol('_settings')] = new Proxy(
-			baseObject,
-			dataHandler
-		);
-
-		return this.settings;
-	}
-
-	return Context;
-})();
-*/
-
-class EmphSettings {
+export default class EmphSettings {
 	store: Map<string, any>;
 	settings: ProxyHandler<object>;
 	constructor(Instance, BaseObject: {}) {
@@ -99,6 +46,3 @@ class EmphSettings {
 		);
 	}
 }
-
-
-export default EmphSettings;
