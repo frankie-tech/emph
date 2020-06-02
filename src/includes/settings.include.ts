e08b7091497b8df8d707472ddd4dfd850b16280e
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-02 00:45:36 -0400

    💄 adds types for proxy handlers
    now includes types
    - EmphSettingsKey
      restricts the key to being only a string or a number to
      avoid symbols being used
    - EmphSettingValue
      Lists the valid values for the proxyhandler
    - EmphSettingTarget
      Meant more as an interface? Maybe this needs to be changed
    
    also fixes the errors that came after adding those types
    such as:
    Numbers don't include indexOf
    - fixed by adding toString()
    Can't use delete operator
    - switches to deleteProperty()
    Error on obj[key]
    - switched to Object.defineProperty
    - this may need to be changed and
       researched
    
    Other notable changes:
    Removed internal references
    - no longer uses self.settings or self.store
    - instead it automatically applies to the Instance passed
    - As symbols those settings are visible
    - - need to make sure they are still accessible
    
    Overall this was a successful conversion
    And I would say I was able to solve issues that
    existed on the pre-class version
    
    things to try to add/fix moving forward
    - add more traps
    - include internal accessors when necessary

--- src/includes/settings.include.ts
+++ src/includes/settings.include.ts
@@ -1,48 +1,56 @@
+type EmphSettingKey = string | number;
+type EmphSettingValue = undefined | string | number | symbol;
+type EmphSettingTarget = {
+    [key in EmphSettingKey]: any;
+};
+
 export default class EmphSettings {
-	store: Map<string, any>;
-	settings: ProxyHandler<object>;
-	constructor(Instance, BaseObject: {}) {
-		let _store = Symbol();
-		let _settings = Symbol()
-		let self = this;
-		const _data = function (): ProxyHandler<object> {
-			return {
-				get: function (obj: { [key: string]: string | number }, key: string): undefined | string | number {
-					// for now does not support nested objects/arrays
-					// if (['object', 'array'].indexOf(type(obj[key])) > -1) {
-					// return new Proxy(obj[key], data(instance));
-					// }
+    constructor(Instance: any, BaseObject = {}) {
+        let _store = Symbol();
+        let _settings = Symbol()
+        let self = this;
+        const _data = function (): ProxyHandler<object> {
+            return {
+                get: function (target: EmphSettingTarget, key: EmphSettingKey): EmphSettingValue {
+                    // for now does not support nested objects/arrays
+                    // if (['object', 'array'].indexOf(type(obj[key])) > -1) {
+                    // return new Proxy(obj[key], data(instance));
+                    // }
 
-					if (key.indexOf('_') === 0) return undefined;
-					return obj[key];
-				},
-				set: function (obj: { [key: string]: string | number }, key: string, value: string | number): boolean {
-					if (obj[key] === value) return true;
-					if (key.indexOf('_') === 0) {
-						self.store.set(key, value);
-						return true;
-					}
-					obj[key] = value;
-					return true;
-				},
-				deleteProperty: function (obj: { [key: string]: string | number }, key: string) {
-					// looks for a secret key just in case;
-					if (key.indexOf('_') === 0) {
-						return self.store.has(key) ? self.store.delete(key) : false;
-					}
-					if (key in obj) {
-						delete obj[key];
-						return true;
-					}
-					return false;
-				},
-			};
-		};
+                    if (key.toString().indexOf('_') === 0) return undefined;
+                    return target[key];
+                },
+                set: function (obj: EmphSettingTarget, key: EmphSettingKey, value: EmphSettingValue): boolean {
+                    if (obj[key] === value) return true;
+                    if (key.toString().indexOf('_') === 0) {
 
-		self.store = Instance.prototype[_store] = new Map();
-		self.settings = Instance.prototype[_settings] = new Proxy(
-			BaseObject,
-			_data()
-		);
-	}
+                        Instance[_store].set(key.toString(), value);
+                        return true;
+                    }
+                    Object.defineProperty(obj, key, {
+                        value,
+                        writable: true
+                    });
+                    return true;
+                },
+                deleteProperty: function (obj: EmphSettingTarget, key: EmphSettingKey): boolean {
+                    // looks for a secret key just in case;
+                    if (key.toString().indexOf('_') === 0) {
+                        return Instance[_store].has(key.toString()) ? Instance[_store].delete(key.toString()) : false;
+                    }
+                    if (key in obj) {
+                        obj.deleteProperty(key);
+                        return true;
+                    }
+                    return false;
+                },
+            };
+        };
+
+        Instance[_store] = new Map();
+        Instance[_settings] = new Proxy(
+            BaseObject,
+            _data()
+        );
+    }
 }
