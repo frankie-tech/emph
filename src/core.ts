c2fec88cea2de492772508725f4e5f5b68eedba2
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-01 21:08:28 -0400

    💄 removes a lot of old code
    includes new messages class
    creates the new Emph class that extends the
    EmphBase class

--- src/core.ts
+++ src/core.ts
@@ -1,22 +1,25 @@
 'use strict';
 interface _ { };
-import './type';
-import ready from './includes/ready.include';
+import type from './type.d';
+// import ready from './includes/ready.include';
 // import type from './includes/type.include';
-import listen from './includes/listen.include';
-import { dispatch } from './includes/message.include';
+// import listen from './includes/listen.include';
+// import { dispatch } from './includes/message.include';
 // import Settings from './includes/settings.include';
 // import empty from './includes/empty.include';
 import Settings from './includes/settings.include';
 import EmphEvents from './includes/event.include';
+import EmphMessages from './includes/message.include';
 /*
 import {
 	requestIdle,
 	cancelIdle,
 } from './polyfills/requestIdleCallback.polyfill';
 */
+/*
 const _ = (() => {
 	'use strict';
+
 
 	const env = {
 		src: 'https://github.com/frankie-tech/emph',
@@ -25,39 +28,21 @@ const _ = (() => {
 
 	function Emph() { }
 
-	// imported functions
-	/**
-	 * @param {EventTarget} eventTarget
-	 * @param {string} eventName
-	 * @param {EventListener} eventHandler
-	 * @param {object} options
-	 */
-	Emph.listen = (eventTarget, eventName, eventHandler, options) =>
-		listen(eventTarget, eventName, eventHandler, options);
-
-	/**
-	 * @param {string} eventName - either system or custom event name
-	 * @param {object} detail - detail object to assign to a custom event
-	 */
-	Emph.dispatch = (eventName, detail) => dispatch(eventName, detail);
-	/**
-	 * @param {EventListener} func - callback function for load event
-	 * @param {boolean} win - should the load event be assigned to window | document
-	 */
-	Emph.ready = (func, win) => ready(func, win);
-
 	Emph.env = Settings(Emph, env);
 
 	return Emph;
 })();
+*/
 
 class EmphBase {
 	events = new EmphEvents();
-
+	messages = new EmphMessages();
 	constructor() { }
+
 	type(variable: any): string {
 		return Object.prototype.toString.call(variable).slice(8, -1).toLowerCase();
 	}
+
 	distinct() {
 		return Array.from(new Set(Array.from(arguments)));
 	}
@@ -91,5 +76,13 @@ class EmphBase {
 	}
 }
 
+class Emph extends EmphBase {
+	constructor() {
+		super();
+	}
+
+
+}
+
 // https://github.com/shichuan/javascript-patterns/blob/master/function-patterns/immediate-object-initialization.html
-export default _;
+export default Emph;
