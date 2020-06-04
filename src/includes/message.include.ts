626b5abae7a13a22506e606080287c75f6a4e5ea
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-03 22:29:20 -0400

    marks as safe to delete

--- src/includes/message.include.ts
+++ src/includes/message.include.ts
@@ -1,36 +1,31 @@
+//! safe to delete
 export default class EmphMessages {
-	styles: string;
-	successStyle: string;
-	warningStyle: string;
-	errorStyle: string;
-	constructor() {
-		this.styles = `
-			display: inline-block;
-			padding-top: 10px;
-			padding-right: 15px;
-			padding-bottom: 10px;
-			padding-left: 15px;
-			font-size: 18px;
-		`;
-		this.successStyle = `color: rgba(132, 203, 77);background-color: #287c03;`
-		this.warningStyle = ``;
-		this.errorStyle = ``;
-	}
+    styles: string;
+    successStyle: string;
+    warningStyle: string;
+    errorStyle: string;
+    constructor() {
+        this.styles = `display: inline-block;padding-top: 10px;padding-right: 15px;padding-bottom: 10px;padding-left: 15px;font-size: 18px;`;
+        this.successStyle = `color: rgba(132, 203, 77);background-color: #287c03;`
+        this.warningStyle = ``;
+        this.errorStyle = ``;
 
-	format(style: string): string {
-		return this.styles.replace(/[\r\s\n]+/gm, '').trim() + style;
-	}
+    }
 
-	success(message: string): void {
-		const style = this.format(this.successStyle)
-		console.log('%cSuccess ' + message, style);
-	}
+    format(style: string): string {
+        return this.styles.replace(/[\r\s\n]+/gm, '').trim() + style;
+    }
 
-	warn(warning: string): void {
-		console.warn('❗️Warning::' + warning);
-	}
+    success(message: string): void {
+        const style = this.format(this.successStyle)
+        console.log('%cSuccess ' + message, style);
+    }
 
-	err(errorName: string): never {
-		throw new Error('🚨Error::' + errorName);
-	}
+    warn(warning: string): void {
+        console.warn('❗️Warning::' + warning);
+    }
+
+    err(errorName: string): never {
+        throw new Error('🚨Error::' + errorName);
+    }
 }
