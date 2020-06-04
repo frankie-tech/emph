16abbf46401517afbf9b33933d8ad6aa36df973d
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-04 13:41:29 -0400

    removes all files that are no longer necessary
    These files have been merged into other classes
    Or not needed
    
    One example is ready.include.js
    
    this is only necessary for DOMContentLoaded or window.onload
    events. This can be substituted though with
    the pubsub class made
    
    ```js
    document.addEventListener(
        'DOMContentLoaded',
        _.emit('DOMLoaded'),
        { once: true }
    );
    _.on('DOMLoaded', callBackOnLoadFunction);
    _on('DOMLoaded', differentOnLoadFunc);
    ```
    
    this gives me the same exact benefits
    but also cuts out some of the cruft
    
    others like empty, type, and request idle polyfill
    have been merged into EmphBase
    
    message.include has been cut entirely
    it was a cute idea, but was just not necessary

--- src/includes/listen.include.js
+++
@@ -1,7 +0,0 @@
-//! Safe to delete
-export default function (eventTarget, eventName, eventHandler, options = {}) {
-    if (!eventTarget)
-        return document.addEventListener(eventName, eventHandler, options);
-
-    eventTarget.addEventListener(eventName, eventHandler, options);
-}
--- src/includes/type.include.ts
+++
@@ -1,5 +0,0 @@
-//! safe to delete
-// returns the true type of a variable
-export default function (variable: any): string {
-    return Object.prototype.toString.call(variable).slice(8, -1).toLowerCase();
-}
