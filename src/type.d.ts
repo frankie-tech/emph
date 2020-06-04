6936d97a46107e8ffcefff3f8f899f7d4ae5eec6
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-03 22:22:26 -0400

    💄imported types from various parts
    Things were a little cluttered on
    other pages so I found it easier to
    merge them all together
    I also used namespaces to help a little bit

--- src/type.d.ts
+++ src/type.d.ts
@@ -8,6 +8,8 @@ declare global {
     }
 }
 
+export default global;
+
 type RequestIdleCallbackHandle = any;
 type RequestIdleCallbackOptions = {
     timeout: number;
@@ -18,4 +20,66 @@ type RequestIdleCallbackDeadline = {
     timeRemaining: (() => number);
 };
 
-export default global;
+export interface Base {
+    type: (variable: any) => string;
+    distinct: (list: Array<any>) => Array<any>;
+    empty: (variable: any) => boolean;
+    requestIdle: (cb: Function) => void;
+    cancelIdle: (id: number) => void;
+}
+
+
+export interface Instance {
+    handlers: Event.Emitters;
+    _store: Settings.Store;
+    _settings: object;
+    knowinglyUpdateOrCreateSettings: (setting: Settings.Key, value: Settings.Value) => boolean;
+}
+
+/* Settings Types */
+export interface SettingsBase {
+    Instance: Instance;
+    _settings: Settings.Target;
+    _store: Settings.Store;
+}
+
+export namespace Settings {
+    export type Key = string | number;
+    export type Value = undefined | string | number | symbol;
+    export type Target = {
+        [key in Settings.Key]: Settings.Value;
+    };
+    export type Store = Map<Settings.Key, Settings.Value>;
+}
+
+
+/*
+export {
+    Settings,
+    settingKey,
+    settingValue,
+    settingTarget,
+    settingStore
+}
+*/
+/* Event Types */
+export namespace Event {
+    export type Type = string;
+    export type Handler = Function;
+    export type Handlers = Array<Handler>;
+    export type Emitters = Map<Type, Handlers>;
+}
+
+/*
+type eventType = string;
+type eventHandler = Function;
+type eventHandlers = Array<eventHandler>;
+type eventEmitters = Map<eventType, eventHandlers>;
+
+export {
+    eventType,
+    eventHandler,
+    eventHandlers,
+    eventEmitters,
+}
+*/
