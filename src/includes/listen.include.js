21216bfacf92f915c97482bc71f8a299d42305d6
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-03 22:20:45 -0400

    marked as safe to delete

--- src/includes/listen.include.js
+++ src/includes/listen.include.js
@@ -1,6 +1,7 @@
+//! Safe to delete
 export default function (eventTarget, eventName, eventHandler, options = {}) {
-	if (!eventTarget)
-		return document.addEventListener(eventName, eventHandler, options);
+    if (!eventTarget)
+        return document.addEventListener(eventName, eventHandler, options);
 
-	eventTarget.addEventListener(eventName, eventHandler, options);
+    eventTarget.addEventListener(eventName, eventHandler, options);
 }
