32f03ec7ec32ee5c89295a39c7cddc5d710827d7
frankie <26461046+frankiekie@users.noreply.github.com>
2020-08-16 15:59:55 -0400

    events rewrite

--- src/includes/event.include.ts
+++ src/includes/event.include.ts
@@ -3,34 +3,29 @@ import { Event } from '../type';
 
 
 export default class EmphEvents {
-    handlers: Event.Emitters;
-    Instance: Instance;
-    constructor(Instance: Instance) {
-        Instance.handlers = Instance.handlers || new Map();
-        this.Instance = Instance
-    }
+	handlers: Event.Emitters;
+	constructor(Instance: Instance) {
+		this.handlers = Instance.handlers || new Map();
+	}
 
-    on(eventType: Event.Type, eventHandler: Event.Handler): void {
-        let currentHandlers = this.Instance.handlers.get(eventType) || [];
+	on(eventType: Event.Type, eventHandler: Event.Handler): void {
+		let currentHandlers = this.handlers.get(eventType) || [];
 
-        if (!currentHandlers.length) {
-            currentHandlers.push(eventHandler);
-            this.Instance.handlers.set(eventType, [eventHandler]);
-        }
-    }
+		if (!currentHandlers.length) {
+			currentHandlers.push(eventHandler);
+			this.handlers.set(eventType, [eventHandler]);
+		}
+	}
 
-    off(eventType: Event.Type, eventHandler: Event.Handler): void {
-        let currentHandlers = this.Instance.handlers.get(eventType);
-        if (currentHandlers) {
-            currentHandlers.splice(currentHandlers.indexOf(eventHandler) >>> 0, 1);
-        }
-    }
+	off(eventType: Event.Type, eventHandler: Event.Handler): void {
+		let currentHandlers = this.handlers.get(eventType);
+		if (currentHandlers) {
+			currentHandlers.splice(currentHandlers.indexOf(eventHandler) >>> 0, 1);
+		}
+	}
 
-    emit(eventType: Event.Type, eventData: {}): void {
-        let eventHandlersForType = this.Instance.handlers.get(eventType) || [];
-        let eventHandlersForAny = this.Instance.handlers.get('*') || [];
-
-        eventHandlersForType.slice().map(handler => handler(eventData));
-        eventHandlersForAny.slice().map(handler => handler(eventType, eventData))
-    }
+	emit(eventType: Event.Type, eventData: {}): void {
+		(this.handlers.get(eventType) || []).slice().map(handler => handler(eventData));
+		(this.handlers.get('*') || []).slice().map(handler => handler(eventType, eventData))
+	}
 }
