ed57178314ceb2bbf7aeedac60587650a47d501b
frankie <26461046+frankiekie@users.noreply.github.com>
2021-02-23 14:23:34 -0500

    💄 Adds empty util

--- src/core.ts
+++ src/core.ts
@@ -18,9 +18,9 @@ class RockbellBase implements Base {
 	// checks if the variable is equal to a false/empty value
 	empty(variable: any): boolean {
 		if ([undefined, null, false, 0, '', '0'].includes(variable)) return true;
-		if (variable.pop) return variable.length === 0 ? true : false;
-		if (variable instanceof Object)
-			return Object.entries(variable).length === 0 ? true : false;
+		if (variable.pop) return variable.length === 0;
+		if (variable.keys)
+			return Object.keys(variable).length === 0;
 
 		return false;
 	}
